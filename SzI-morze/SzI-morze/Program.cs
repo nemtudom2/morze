cfff5c918b118583f74944c90c69f9fc0e96671d
nemtudom2 <szabo.istvan@diak.szbi-pg.hu>
2025-02-11 12:02:31 +0100

    elso

---
+++ SzI-morze/SzI-morze/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzI_morze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new StreamReader("../../morzeabc.txt").ReadToEnd().Replace("\r\n", "\n").Split('\n').ToList();
+            a.RemoveAt(0);
+            Dictionary<string, string> abc = new Dictionary<string, string>();
+            foreach (string line in a)
+            {
+                abc[line.Split('\t')[1]] = line.Split('\t')[0];
+            }
+            Console.WriteLine($"A file {abc.Count()} elemet tartalmaz");
+            Console.Write($"kerek egy karaktert: ");
+            var b = Console.ReadLine();
+            Console.WriteLine(abc.ContainsKey(b)? abc[b] : "nincs ilyen karakter");
+        }
+    }
+}
