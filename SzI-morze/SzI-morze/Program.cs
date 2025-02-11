9b0c34bddf41940d283c532e84b5e4d54e180072
nemtudom2 <szabo.istvan@diak.szbi-pg.hu>
2025-02-11 12:35:53 +0100

    sadf

--- SzI-morze/SzI-morze/Program.cs
+++ SzI-morze/SzI-morze/Program.cs
@@ -14,15 +14,42 @@ namespace SzI_morze
             var a = new StreamReader("../../morzeabc.txt").ReadToEnd().Replace("\r\n", "\n").Split('\n').ToList();
             a.RemoveAt(0);
             Dictionary<string, string> abc = new Dictionary<string, string>();
+            Dictionary<string, string> megfejtoabc = new Dictionary<string, string>();
             foreach (string line in a)
             {
                 abc[line.Split('\t')[0].ToLower()] = line.Split('\t')[1];
                 abc[line.Split('\t')[0].ToUpper()] = line.Split('\t')[1];
+                megfejtoabc[line.Split('\t')[1].ToUpper()] = line.Split('\t')[0].ToUpper();
             }
             Console.WriteLine($"A file {abc.Count()} elemet tartalmaz");
             Console.Write($"kerek egy karaktert: ");
             var b = Console.ReadLine();
             Console.WriteLine(abc.ContainsKey(b)? abc[b] : "nincs ilyen karakter");
+            
+            IEnumerable<List<List<string>>> dekodolando = from y in (
+                      from x in (new StreamReader("../../morze.txt").ReadToEnd().Replace("\t", "").Split('\n').ToList()) select x.Replace("       ", ":").Split(':').ToList())
+                                select y.Select(x=>x.Replace("   ", ":").Split(':').ToList()).ToList();
+            string dekodolt = "";
+            foreach (var line in dekodolando)
+            {
+                foreach (var szo in line)
+                {
+                    foreach (var betu in szo)
+                    {
+                        try
+                        {
+                            dekodolt += megfejtoabc[betu];
+                        }
+                        catch
+                        {
+                            Console.WriteLine(betu);
+                        }
+                    }
+                    dekodolt += " ";
+                }
+                dekodolt+="\n";
+            }
+            Console.WriteLine(dekodolt);
             Console.ReadLine();
         }
     }
